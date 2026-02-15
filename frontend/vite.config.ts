6bcbc052931acd213050b52e173146c3d4b0ee0b
Việt Anh <vanh1200@Vanh1200.local>
2026-02-15 10:27:03 +0700

    update config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:3001',
-  //       changeOrigin: true,
-  //     }
-  //   }
-  // }
-})
+  server: command === 'serve' ? {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
+    }
+  } : undefined,
+}))
